9d09531d55e8fa47eca4e231fcd385469309e49b
Indira Nafa <indiranafa24@gmail.com>
2025-02-24 20:53:42 +0700

    Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(){
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user.index', ['user' => $user]);
+    }
+}
